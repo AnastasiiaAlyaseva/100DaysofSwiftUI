245294ccf29184cdb48050d1a02b3ea5cce97b29
Anastasiia Alyaseva <180915an@gmail.com>
2024-03-21 17:21:14 +0100

    Challenge

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -4,9 +4,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var showingFinalScore = false
     @State private var scoreTitle = ""
-    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
-    @State var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var currentScore = 0
+    @State private var questionCount = 0
+    
     
     var body: some View {
         ZStack{
@@ -35,6 +39,7 @@ struct ContentView: View {
                     ForEach(0..<3) { number in
                         Button {
                             flagTapped(number)
+                            scoreСalculation(number)
                         } label: {
                             Image(countries[number])
                                 .clipShape(.capsule)
@@ -50,7 +55,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(currentScore)")
                     .font(.title.bold())
                     .foregroundStyle(.white)
                 
@@ -58,23 +63,52 @@ struct ContentView: View {
             }
             .padding()
         }
+        
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
         } message: {
-            Text("You score is ???")
+            Text("You score is \(currentScore)")
+        }
+        .alert("Finale Score", isPresented: $showingFinalScore) {
+            Button("Restart", action: restartGame)
+        } message: {
+            Text(" Congratulations! You answered \(currentScore) out of 8 questions correctly!")
         }
     }
+    
     func flagTapped(_ number: Int){
         if number == correctAnswer{
             scoreTitle = "Correct"
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's tha flag of \(countries[number])"
         }
         showingScore = true
+        questionCount += 1
     }
+    
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        if questionCount < 8 {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        } else {
+            showingFinalScore = true
+        }
+    }
+    
+    func scoreСalculation(_ number: Int) {
+        if number == correctAnswer {
+            currentScore += 1
+        } else {
+            currentScore -= 1
+        }
+    }
+    func restartGame() {
+        currentScore = 0
+        questionCount = 0
+        scoreTitle = ""
+        showingScore = false
+        showingFinalScore = false
+        
     }
 }
 
