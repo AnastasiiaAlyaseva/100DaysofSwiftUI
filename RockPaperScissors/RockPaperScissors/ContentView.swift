e3a851b00b89747df5ee83264898ee3ef113d769
Anastasiia Alyaseva <180915an@gmail.com>
2024-03-25 19:08:14 +0100

    Milestone: Projects 1-3

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -2,9 +2,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var moves = ["Rock", "Paper", "Scissors"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var appChoice = Int.random(in: 0...2)
+    @State private var shouldWin = true
+    @State private var currantScore = 0
+    @State private var showingScore = false
+    @State private var showingFinalScore = false
+    @State private var scoreTitle = ""
+    @State private var roundCount = 0
     
+    let moves = ["Rock", "Paper", "Scissors"].shuffled()
     
     
     var body: some View {
@@ -16,17 +22,17 @@ struct ContentView: View {
                 
                 VStack(spacing: 20) {
                     VStack {
-                        Text("Make your choice")
+                        Text("App choice")
                             .foregroundStyle(.white)
                             .font(.subheadline.weight(.heavy))
-                        Text(moves[correctAnswer])
+                        Text(moves[appChoice])
                             .font(.largeTitle.weight(.semibold))
                     }
                     
                     HStack {
                         ForEach(0..<3) { number in
-                            Button {
-                                //
+                            Button{
+                                playRound(number)
                             } label: {
                                 Image(moves[number])
                                     .clipShape(.circle)
@@ -40,8 +46,62 @@ struct ContentView: View {
                     .clipShape(.rect(cornerRadius: 20))
                     
                 }
+                Text("Your score: \(currantScore)")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: resetRound)
+        } message: {
+            Text ("Your score is \(currantScore)")
+        }
+        .alert("Final score", isPresented: $showingFinalScore) {
+            Button("New game" , action: restartGame)
+        } message: {
+            Text("Congratulations! You got \(currantScore) point")
+        }
+    }
+    
+    func playRound(_ playerChoice: Int) {
+        let winConditions = [[0, 1], [1, 2], [2, 0]]
+        var isCorrect = false
+        if shouldWin {
+            isCorrect = winConditions[appChoice].contains(playerChoice)
+        } else {
+            isCorrect = appChoice == playerChoice
+        }
+        
+        showingScore = true
+        roundCount += 1
+        scoreCalculation(isCorrect)
+    }
+    
+    func resetRound () {
+        if roundCount < 10 {
+            appChoice = Int.random(in: 0...2)
+        
+        } else {
+            showingFinalScore = true
+        }
+    }
+    
+    func scoreCalculation(_ isCorrect: Bool) {
+        if isCorrect {
+            currantScore += 1
+            scoreTitle = "Correct"
+        } else {
+            currantScore -= 1
+            scoreTitle = "Wrong"
+        }
+    }
+    
+    func restartGame() {
+        currantScore = 0
+        roundCount = 0
+        scoreTitle = ""
+        showingScore = false
+        showingFinalScore = false
     }
 }
 
