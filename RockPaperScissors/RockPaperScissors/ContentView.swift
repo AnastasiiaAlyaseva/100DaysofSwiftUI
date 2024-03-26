da53f9f9fe92e274794403ed3037748440d3ba51
Anastasiia Alyaseva <180915an@gmail.com>
2024-03-26 13:22:18 +0100

    created modifier

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -1,9 +1,34 @@
 
 import SwiftUI
 
+struct GameImage: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 50)
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 30))
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title.bold())
+            .foregroundStyle(.white)
+    }
+}
+
+extension View {
+    func titleWhiteStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+
 struct ContentView: View {
     @State private var appChoice = Int.random(in: 0...2)
-    @State private var shouldWin = true
+    @State private var shouldWin = Bool.random()
     @State private var currantScore = 0
     @State private var showingScore = false
     @State private var showingFinalScore = false
@@ -20,13 +45,20 @@ struct ContentView: View {
             
             VStack{
                 
+                Text("Rock Paper Scissors Game")
+                    .titleWhiteStyle()
+           
                 VStack(spacing: 20) {
+        
+                    Spacer()
+                    
                     VStack {
-                        Text("App choice")
-                            .foregroundStyle(.white)
+                        Text("App choice:")
+                            .foregroundStyle(.secondary)
                             .font(.subheadline.weight(.heavy))
                         Text(moves[appChoice])
                             .font(.largeTitle.weight(.semibold))
+                        
                     }
                     
                     HStack {
@@ -40,15 +72,18 @@ struct ContentView: View {
                             }
                         }
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 20)
-                    .background(.regularMaterial)
-                    .clipShape(.rect(cornerRadius: 20))
+                    .modifier(GameImage())
                     
+                    Text("You need to \(shouldWin ? "win" : "lose") !")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                        
+                    Spacer()
                 }
+
                 Text("Your score: \(currantScore)")
-                    .font(.largeTitle.bold())
-                    .foregroundStyle(.white)
+                    .titleWhiteStyle()
+                    
             }
         }
         .alert(scoreTitle, isPresented: $showingScore) {
@@ -65,7 +100,7 @@ struct ContentView: View {
     
     func playRound(_ playerChoice: Int) {
         let winConditions = [[0, 1], [1, 2], [2, 0]]
-        var isCorrect = false
+        var isCorrect = true
         if shouldWin {
             isCorrect = winConditions[appChoice].contains(playerChoice)
         } else {
@@ -80,14 +115,14 @@ struct ContentView: View {
     func resetRound () {
         if roundCount < 10 {
             appChoice = Int.random(in: 0...2)
-        
+            shouldWin.toggle()
         } else {
             showingFinalScore = true
         }
     }
     
     func scoreCalculation(_ isCorrect: Bool) {
-        if isCorrect {
+        if !isCorrect {
             currantScore += 1
             scoreTitle = "Correct"
         } else {
---
+++ RockPaperScissors/RockPaperScissors/Move.swift
@@ -0,0 +1,8 @@
+//
+//  Move.swift
+//  RockPaperScissors
+//
+//  Created by Anastasiia Alyaseva on 26.03.2024.
+//
+
+import Foundation
