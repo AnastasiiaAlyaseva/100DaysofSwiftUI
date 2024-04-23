84f38dbc0f79456423c706475ffdfc53eabe7b91
Anastasiia Alyaseva <180915an@gmail.com>
2024-04-23 15:38:50 +0200

    GeometryReader

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -11,68 +11,43 @@ struct MissionView: View {
     let crew: [CrewMembr]
     
     var body: some View {
-        ScrollView{
-            VStack{
-                Image(mission.image)
-                    .resizable()
-                    .scaledToFit()
-                    .containerRelativeFrame(.horizontal) { width, axis in
-                        width * 0.6
-                    }
-                    .padding(.top)
-                
-                Text(mission.formattedLaunchDate)
-                    .font(.title3)
-                    .foregroundStyle(.secondary)
-                    .padding(.top)
-                
-                VStack(alignment: .leading) {
-                    DividerView()
-                    
-                    Text("Mission Highlighs")
-                        .font(.title.bold())
-                        .padding(.bottom, 5)
-                    
-                    Text(mission.description)
-                    
-                    DividerView()
-                    
-                    Text("Crew")
-                        .font(.title.bold())
-                        .padding(.bottom, 5)
-                }
-                .padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(crew, id: \.role) { crewMember in
-                            NavigationLink{
-                                AstronautView(astronaut: crewMember.astronaut)
-                            } label: {
-                                HStack{
-                                    Image(crewMember.astronaut.id)
-                                        .resizable()
-                                        .frame(width: 104, height: 72)
-                                        .clipShape(.capsule)
-                                        .overlay(
-                                            Capsule()
-                                                .strokeBorder(.white, lineWidth: 1)
-                                        )
-                                    VStack(alignment: .leading) {
-                                        Text(crewMember.astronaut.name)
-                                            .foregroundStyle(.white)
-                                            .font(.headline)
-                                        Text(crewMember.role)
-                                            .foregroundStyle(.white.opacity(0.5))
-                                    }
-                                }
-                                .padding(.horizontal)
-                            }
+        GeometryReader { geometry in
+            ScrollView{
+                VStack{
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .containerRelativeFrame(.horizontal) { width, axis in
+                            width * 0.6
                         }
+                        .padding(.top)
+                    
+                    Text(mission.formattedLaunchDate)
+                        .font(.title3)
+                        .foregroundStyle(.secondary)
+                        .padding(.top)
+                    
+                    VStack(alignment: .leading) {
+                        DividerView()
+                        
+                        Text("Mission Highlighs")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
+                        Text(mission.description)
+                        
+                        DividerView()
+                        
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
                     }
+                    .padding(.horizontal)
+                    
+                    CrewView(crew: crew)
                 }
+                .padding(.bottom)
             }
-            .padding(.bottom)
         }
         .navigationTitle(mission.displayName)
         .navigationBarTitleDisplayMode(.inline)
