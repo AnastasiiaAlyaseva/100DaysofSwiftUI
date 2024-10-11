21148a75bcea775e5dcfbc8343daa7d7d771441c
Anastasiia Alyaseva <180915an@gmail.com>
2024-10-11 18:54:08 +0200

    Designed card view

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "What is the capital city of Scotland?", answer: "Edinburgh")
+}
---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,13 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView(card: .example)
+    }
+}
+
+#Preview {
+    ContentView()
+}
