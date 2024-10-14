a23099d58f4d35a620f1d80e97f84b47678a213a
Anastasiia Alyaseva <180915an@gmail.com>
2024-10-14 18:22:52 +0200

    colored views

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -2,6 +2,7 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     
@@ -11,7 +12,16 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .fill(.white)
+                .fill( accessibilityDifferentiateWithoutColor
+                       ? .white
+                       : .white
+                    .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background( accessibilityDifferentiateWithoutColor
+                             ? nil
+                             : RoundedRectangle(cornerRadius: 25)
+                    .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
             VStack {
